419a255d046ab82eb261077effd0f46f4b05d56e
Chienhui Lee <leech.developer@gmail.com>
2022-07-21 08:32:38 -0600

    feat: add unit test for logic issue, fix cancellation registry contract

--- test/exchangev4.js
+++ test/exchangev4.js
@@ -41,12 +41,14 @@ describe("NFTMarketPlace", function () {
       { name: "paymentERC20", type: "address" }
     ]
   };
+  let currentTime;
 
   beforeEach(async function() {
     [owner, addr1, maker] = await ethers.getSigners();
     const ExchangeV4 = await ethers.getContractFactory("ExchangeV4");
     marketplace = await ExchangeV4.deploy();
     await marketplace.deployed();
+    currentTime = Math.floor(Date.now() / 1000) - 100;
   });
 
   it("Change Owner of MarketPlace", async function () {
@@ -92,18 +94,17 @@ describe("NFTMarketPlace", function () {
         cancellationRegistry.address,
         paymentERC20Registry.address
       );
+      currentTime = Math.floor(Date.now() / 1000) - 100;
     })
 
     it("Check ETH amount of the transaction", async function () {
-      // Todo Write testcase to fillSellOrder Method
-      // SellOrder(,address paymentERC20)
       const currentBlockNumber = await ethers.provider.getBlockNumber();
       const sellOrder = {
         seller: owner.address,
         contractAddress: testNFT.address,
         tokenId: 0,
-        startTime: 1658338153,
-        expiration: 1658938153,
+        startTime: currentTime,
+        expiration: currentTime + 10 ** 6,
         price: 5,
         quantity: 1,
         createdAtBlockNumber: currentBlockNumber,
@@ -121,8 +122,8 @@ describe("NFTMarketPlace", function () {
         owner.address,
         testNFT.address,
         0,
-        1658338153,
-        1658938153,
+        currentTime,
+        currentTime + 10 ** 6,
         5,
         1,
         currentBlockNumber,
@@ -133,15 +134,13 @@ describe("NFTMarketPlace", function () {
     });
 
     it("Check ERC20 token approval by Registry", async function () {
-      // Todo Write testcase to fillSellOrder Method
-      // SellOrder(,address paymentERC20)
       const currentBlockNumber = await ethers.provider.getBlockNumber();
       const sellOrder = {
         seller: owner.address,
         contractAddress: testNFT.address,
         tokenId: 0,
-        startTime: 1658338153,
-        expiration: 1658938153,
+        startTime: currentTime,
+        expiration: currentTime + 10 ** 6,
         price: utils.parseEther('10'),
         quantity: 1,
         createdAtBlockNumber: currentBlockNumber,
@@ -159,8 +158,8 @@ describe("NFTMarketPlace", function () {
         owner.address,
         testNFT.address,
         0,
-        1658338153,
-        1658938153,
+        currentTime,
+        currentTime + 10 ** 6,
         utils.parseEther('10'),
         1,
         currentBlockNumber,
@@ -171,15 +170,13 @@ describe("NFTMarketPlace", function () {
     });
 
     it("Check ERC20 token balance of buyer", async function () {
-      // Todo Write testcase to fillSellOrder Method
-      // SellOrder(,address paymentERC20)
       const currentBlockNumber = await ethers.provider.getBlockNumber();
       const sellOrder = {
         seller: owner.address,
         contractAddress: testNFT.address,
         tokenId: 0,
-        startTime: 1658338153,
-        expiration: 1658938153,
+        startTime: currentTime,
+        expiration: currentTime + 10 ** 6,
         price: utils.parseEther('10'),
         quantity: 1,
         createdAtBlockNumber: currentBlockNumber,
@@ -198,8 +195,8 @@ describe("NFTMarketPlace", function () {
         owner.address,
         testNFT.address,
         0,
-        1658338153,
-        1658938153,
+        currentTime,
+        currentTime + 10 ** 6,
         utils.parseEther('10'),
         1,
         currentBlockNumber,
@@ -210,15 +207,13 @@ describe("NFTMarketPlace", function () {
     });
   
     it("Check ERC20 token approval from buyer to exchange", async function () {
-      // Todo Write testcase to fillSellOrder Method
-      // SellOrder(,address paymentERC20)
       const currentBlockNumber = await ethers.provider.getBlockNumber();
       const sellOrder = {
         seller: owner.address,
         contractAddress: testNFT.address,
         tokenId: 0,
-        startTime: 1658338153,
-        expiration: 1658938153,
+        startTime: currentTime,
+        expiration: currentTime + 10 ** 6,
         price: utils.parseEther('10'),
         quantity: 1,
         createdAtBlockNumber: currentBlockNumber,
@@ -238,8 +233,8 @@ describe("NFTMarketPlace", function () {
         owner.address,
         testNFT.address,
         0,
-        1658338153,
-        1658938153,
+        currentTime,
+        currentTime + 10 ** 6,
         utils.parseEther('10'),
         1,
         currentBlockNumber,
@@ -250,14 +245,13 @@ describe("NFTMarketPlace", function () {
     });
 
     it("Check signature validation", async function () {
-      // Todo Write testcase to fillSellOrder Method
       const currentBlockNumber = await ethers.provider.getBlockNumber();
       const sellOrder = {
         seller: owner.address,
         contractAddress: testNFT.address,
         tokenId: 0,
-        startTime: 1658338153,
-        expiration: 1658938153,
+        startTime: currentTime,
+        expiration: currentTime + 10 ** 6,
         price: utils.parseEther('10'),
         quantity: 1,
         createdAtBlockNumber: currentBlockNumber,
@@ -278,8 +272,8 @@ describe("NFTMarketPlace", function () {
         owner.address,
         testNFT.address,
         1,
-        1658338153,
-        1658938153,
+        currentTime,
+        currentTime + 10 ** 6,
         utils.parseEther('10'),
         1,
         currentBlockNumber,
@@ -289,16 +283,21 @@ describe("NFTMarketPlace", function () {
       )).to.be.revertedWith("Signature is not valid for SellOrder.");
     });
 
-    it("Check signature validation", async function () {
-      // Todo Write testcase to fillSellOrder Method
+    it("Check cancelled order", async function () {
+      await cancellationRegistry.addRegistrant(owner.address);
+      await cancellationRegistry.cancelPreviousSellOrders(
+        owner.address,
+        testNFT.address,
+        0
+      );
 
       const currentBlockNumber = await ethers.provider.getBlockNumber();
       const sellOrder = {
         seller: owner.address,
         contractAddress: testNFT.address,
         tokenId: 0,
-        startTime: 1658338153,
-        expiration: 1658938153,
+        startTime: currentTime,
+        expiration: currentTime + 10 ** 6,
         price: utils.parseEther('10'),
         quantity: 1,
         createdAtBlockNumber: currentBlockNumber,
@@ -318,16 +317,313 @@ describe("NFTMarketPlace", function () {
       await expect(marketplace.fillSellOrder(
         owner.address,
         testNFT.address,
-        1,
-        1658338153,
-        1658938153,
+        0,
+        currentTime,
+        currentTime + 10 ** 6,
         utils.parseEther('10'),
         1,
         currentBlockNumber,
         erc20.address,
         signature,
         addr1.address
-      )).to.be.revertedWith("Signature is not valid for SellOrder.");
+      )).to.be.revertedWith("This order has been cancelled.");
+    });
+
+    it("Check sell order time - future start time", async function () {
+      const currentBlockNumber = await ethers.provider.getBlockNumber();
+      const sellOrder = {
+        seller: owner.address,
+        contractAddress: testNFT.address,
+        tokenId: 0,
+        startTime: currentTime + 10 ** 6,
+        expiration: 1669638153,
+        price: utils.parseEther('10'),
+        quantity: 1,
+        createdAtBlockNumber: currentBlockNumber,
+        paymentERC20: erc20.address
+      }
+      const signature = await owner._signTypedData(
+        {
+          name: "Pocket Of Quarters",
+          version: "4"
+        },
+        sellOrderTypes,
+        sellOrder
+      );
+      await paymentERC20Registry.addApprovedERC20(erc20.address);
+      await erc20.mint(addr1.address, initialBalance);
+      await erc20.connect(addr1).approve(marketplace.address, initialBalance);
+      await expect(marketplace.fillSellOrder(
+        owner.address,
+        testNFT.address,
+        0,
+        currentTime + 10 ** 6,
+        1669638153,
+        utils.parseEther('10'),
+        1,
+        currentBlockNumber,
+        erc20.address,
+        signature,
+        addr1.address
+      )).to.be.revertedWith("SellOrder start time is in the future.");
+    });
+
+    it("Check sell order time - expired time", async function () {
+      const currentBlockNumber = await ethers.provider.getBlockNumber();
+      const sellOrder = {
+        seller: owner.address,
+        contractAddress: testNFT.address,
+        tokenId: 0,
+        startTime: 1657338153,
+        expiration: 1657838153,
+        price: utils.parseEther('10'),
+        quantity: 1,
+        createdAtBlockNumber: currentBlockNumber,
+        paymentERC20: erc20.address
+      }
+      const signature = await owner._signTypedData(
+        {
+          name: "Pocket Of Quarters",
+          version: "4"
+        },
+        sellOrderTypes,
+        sellOrder
+      );
+      await paymentERC20Registry.addApprovedERC20(erc20.address);
+      await erc20.mint(addr1.address, initialBalance);
+      await erc20.connect(addr1).approve(marketplace.address, initialBalance);
+      await expect(marketplace.fillSellOrder(
+        owner.address,
+        testNFT.address,
+        0,
+        1657338153,
+        1657838153,
+        utils.parseEther('10'),
+        1,
+        currentBlockNumber,
+        erc20.address,
+        signature,
+        addr1.address
+      )).to.be.revertedWith("This sell order has expired.");
+    });
+
+    it("Check NFT allowance", async function () {
+      await cancellationRegistry.addRegistrant(marketplace.address);
+      const currentBlockNumber = await ethers.provider.getBlockNumber();
+      const sellOrder = {
+        seller: owner.address,
+        contractAddress: testNFT.address,
+        tokenId: 0,
+        startTime: currentTime,
+        expiration: currentTime + 10 ** 6,
+        
+        price: utils.parseEther('10'),
+        quantity: 1,
+        createdAtBlockNumber: currentBlockNumber,
+        paymentERC20: erc20.address
+      }
+      const signature = await owner._signTypedData(
+        {
+          name: "Pocket Of Quarters",
+          version: "4"
+        },
+        sellOrderTypes,
+        sellOrder
+      );
+      await paymentERC20Registry.addApprovedERC20(erc20.address);
+      await erc20.mint(addr1.address, initialBalance);
+      await erc20.connect(addr1).approve(marketplace.address, initialBalance);
+      await expect(marketplace.fillSellOrder(
+        owner.address,
+        testNFT.address,
+        0,
+        currentTime,
+        currentTime + 10 ** 6,
+        utils.parseEther('10'),
+        1,
+        currentBlockNumber,
+        erc20.address,
+        signature,
+        addr1.address
+      )).to.be.revertedWith("The Exchange is not approved to operate this NFT");
+    });
+
+    it("Check NFT Contract validation", async function () {
+      await cancellationRegistry.addRegistrant(marketplace.address);
+      const currentBlockNumber = await ethers.provider.getBlockNumber();
+      const sellOrder = {
+        seller: owner.address,
+        contractAddress: secondERC20.address,
+        tokenId: 0,
+        startTime: currentTime,
+        expiration: currentTime + 10 ** 6,
+        
+        price: utils.parseEther('10'),
+        quantity: 1,
+        createdAtBlockNumber: currentBlockNumber,
+        paymentERC20: erc20.address
+      }
+      const signature = await owner._signTypedData(
+        {
+          name: "Pocket Of Quarters",
+          version: "4"
+        },
+        sellOrderTypes,
+        sellOrder
+      );
+      await paymentERC20Registry.addApprovedERC20(erc20.address);
+      await erc20.mint(addr1.address, initialBalance);
+      await erc20.connect(addr1).approve(marketplace.address, initialBalance);
+      await testNFT.setApprovalForAll(marketplace.address, true);
+      await expect(marketplace.fillSellOrder(
+        owner.address,
+        secondERC20.address,
+        0,
+        currentTime,
+        currentTime + 10 ** 6,
+        utils.parseEther('10'),
+        1,
+        currentBlockNumber,
+        erc20.address,
+        signature,
+        addr1.address
+      )).to.be.revertedWith("We don't recognize the NFT as either an ERC721 or ERC1155.");
+    });
+  
+    it("Successful NFT Transfer", async function () {
+      await cancellationRegistry.addRegistrant(marketplace.address);
+      const currentBlockNumber = await ethers.provider.getBlockNumber();
+      const sellOrder = {
+        seller: owner.address,
+        contractAddress: testNFT.address,
+        tokenId: 0,
+        startTime: currentTime,
+        expiration: currentTime + 10 ** 6,
+        
+        price: utils.parseEther('10'),
+        quantity: 1,
+        createdAtBlockNumber: currentBlockNumber,
+        paymentERC20: erc20.address
+      }
+      const signature = await owner._signTypedData(
+        {
+          name: "Pocket Of Quarters",
+          version: "4"
+        },
+        sellOrderTypes,
+        sellOrder
+      );
+      await paymentERC20Registry.addApprovedERC20(erc20.address);
+      await erc20.mint(addr1.address, initialBalance);
+      await erc20.connect(addr1).approve(marketplace.address, initialBalance);
+      await testNFT.setApprovalForAll(marketplace.address, true);
+      const prevBalance = await erc20.balanceOf(addr1.address);
+      await marketplace.fillSellOrder(
+        owner.address,
+        testNFT.address,
+        0,
+        currentTime,
+        currentTime + 10 ** 6,
+        utils.parseEther('10'),
+        1,
+        currentBlockNumber,
+        erc20.address,
+        signature,
+        addr1.address
+      );
+      const predictBalance = ethers.BigNumber.from(prevBalance).sub(utils.parseEther('10'));
+      expect(await erc20.balanceOf(addr1.address)).to.equal(predictBalance);
+    });
+
+    it("Irregular NFT Transfer - multiple times", async function () {
+      await cancellationRegistry.addRegistrant(marketplace.address);
+      const currentBlockNumber = await ethers.provider.getBlockNumber();
+      let sellOrder = {
+        seller: owner.address,
+        contractAddress: testNFT.address,
+        tokenId: 0,
+        startTime: currentTime,
+        expiration: currentTime + 10 ** 6,
+        
+        price: utils.parseEther('10'),
+        quantity: 1,
+        createdAtBlockNumber: currentBlockNumber,
+        paymentERC20: erc20.address
+      }
+      let signature = await owner._signTypedData(
+        {
+          name: "Pocket Of Quarters",
+          version: "4"
+        },
+        sellOrderTypes,
+        sellOrder
+      );
+      await paymentERC20Registry.addApprovedERC20(erc20.address);
+      await erc20.mint(addr1.address, initialBalance);
+      await erc20.connect(addr1).approve(marketplace.address, initialBalance);
+      await testNFT.setApprovalForAll(marketplace.address, true);
+      await testNFT.mintToken(owner.address);
+      await testNFT.mintToken(owner.address);
+      const prevBalance = await erc20.balanceOf(addr1.address);
+      await marketplace.fillSellOrder(
+        owner.address,
+        testNFT.address,
+        0,
+        currentTime,
+        currentTime + 10 ** 6,
+        utils.parseEther('10'),
+        1,
+        currentBlockNumber,
+        erc20.address,
+        signature,
+        addr1.address
+      );
+      sellOrder.tokenId = 1;
+      signature = await owner._signTypedData(
+        {
+          name: "Pocket Of Quarters",
+          version: "4"
+        },
+        sellOrderTypes,
+        sellOrder
+      );
+      await marketplace.fillSellOrder(
+        owner.address,
+        testNFT.address,
+        1,
+        currentTime,
+        currentTime + 10 ** 6,
+        utils.parseEther('10'),
+        1,
+        currentBlockNumber,
+        erc20.address,
+        signature,
+        addr1.address
+      );
+      sellOrder.tokenId = 2;
+      signature = await owner._signTypedData(
+        {
+          name: "Pocket Of Quarters",
+          version: "4"
+        },
+        sellOrderTypes,
+        sellOrder
+      );
+      await marketplace.fillSellOrder(
+        owner.address,
+        testNFT.address,
+        2,
+        currentTime,
+        currentTime + 10 ** 6,
+        utils.parseEther('10'),
+        1,
+        currentBlockNumber,
+        erc20.address,
+        signature,
+        addr1.address
+      );
+      const predictBalance = ethers.BigNumber.from(prevBalance).sub(utils.parseEther('30'));
+      expect(await erc20.balanceOf(addr1.address)).to.equal(predictBalance);
     });
   })
 });
